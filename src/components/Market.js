39176d2dcdd1699c5a4bdc662d94445775756ad5
kimkwonhee <kimkwon429@naver.com>
2022-10-23 08:55:40 +0900

    async awit 진행

--- src/components/Market.js
+++ src/components/Market.js
@@ -1,15 +1,66 @@
-import React from 'react';
+import React, {useEffect, useReducer} from 'react';
 import MarketTemplats from './MarketTemplats';
 import ItemList from './ItemList';
 import OrderItemList from './OrderItemList'
 import itemlists from '../data/items.json'
 
+//lodding, success, error
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'LOADING':
+      return {
+        loading: true,
+        data: null,
+        error: null
+      }
+    case 'SUCCESS':
+      return {
+        loading: false,
+        data: action.data,
+        error: null
+      }
+    case 'ERROR':
+      return {
+        loading: false,
+        data: null,
+        error: action.error
+      }
+    default: 
+      throw new Error('`${action.type}');
+  }
+}
+
 const Market = () => {
+
+  const [state, dispatch] = useReducer(reducer, {
+    loading: false,
+    data: null,
+    error: null
+  });
+
+  const fetchItems = async () => {
+    dispatch({type: 'LOADING'});
+    try {
+      const response = await itemlists;
+      dispatch({type: 'SUCCESS', data: response});
+    } catch (e){
+      dispatch({type: 'ERROR', error: e});
+    }
+  }
+
+  useEffect(() => {
+    fetchItems();
+  }, [])
+
+  const {loading, data: items, error} = state;
+  if(loading) return <div>로딩중...</div>
+  if(error) return <div>에러 발생</div>
+  if(!items) return null;
 
   return (
     <>
       <MarketTemplats 
-        items={<ItemList itmelist={itemlists}/>} 
+        items={<ItemList itmelist={items}/>} 
         basket={<OrderItemList />} 
       />;
     </>
